8ba35c6a293b3d353b480044c267a188bd0fa4c1
Yiannis Tsiouris <yiannis.tsiouris@gmail.com>
2012-07-03 16:08:01 +0300

    Fixed bug in bencherl after pull --rebase

--- bencherl.hs
+++ bencherl.hs
@@ -6,60 +6,60 @@ import Text.Pandoc
 
 main :: IO ()
 main = hakyll $ do
-	
-	-- Stylesheets.
-	match "css/*.css" $ do
-		route idRoute
-		compile compressCssCompiler
-	match "css/start/*.css" $ do
-		route idRoute
-		compile compressCssCompiler
+        -- Stylesheets.
+        match "css/*.css" $ do
+                route idRoute
+                compile compressCssCompiler
+        match "css/start/*.css" $ do
+                route idRoute
+                compile compressCssCompiler
 
-	-- Stylesheet images.
-	match "css/start/images/*" $ do
-		route idRoute
-		compile copyFileCompiler
-	match "css/*.gif" $ do
-		route idRoute
-		compile copyFileCompiler
- 
-	-- Images.
-	match "images/*" $ do	
-		route idRoute
-		compile copyFileCompiler
-	match "images/graphs/*.png*" $ do
-		route idRoute
-		compile copyFileCompiler
+        -- Stylesheet images.
+        match "css/start/images/*" $ do
+                route idRoute
+                compile copyFileCompiler
+        match "css/*.gif" $ do
+                route idRoute
+                compile copyFileCompiler
 
-	-- Javascript files.
-	match "js/*" $ do
-		route idRoute
-		compile copyFileCompiler
+        -- Images.
+        match "images/*" $ do
+                route idRoute
+                compile copyFileCompiler
+        match "images/graphs/*.png*" $ do
+                route idRoute
+                compile copyFileCompiler
 
-	-- Favicon.
-	match "favicon.ico" $ do
-		route idRoute
-		compile copyFileCompiler
+        -- Javascript files.
+        match "js/*" $ do
+                route idRoute
+                compile copyFileCompiler
 
-	-- Templates.
-	match "templates/*" $ do
-		compile templateCompiler
+        -- Favicon.
+        match "favicon.ico" $ do
+                route idRoute
+                compile copyFileCompiler
 
-	-- Pages.
-	match (list ["index.md", "benchmarks.md", "people.md", "publications.md"]) $ do
-		route $ setExtension "html"
-		compile $ pageCompiler
-			>>> applyTemplateCompiler "templates/default.html"
-			>>> relativizeUrlsCompiler
- 
-    -- How-to page (w/ TOC)
-    match "howto.md" $ do
-            route $ setExtension "html"
-            compile $ pageCompilerWith defaultHakyllParserState withToc
-                    >>> applyTemplateCompiler "templates/default.html"
-                    >>> relativizeUrlsCompiler
+        -- Templates.
+        match "templates/*" $ do
+                compile templateCompiler
 
-	match "results.md" $ do
+        -- Pages.
+        match (list ["index.md", "benchmarks.md", "people.md", "publications.md"]) $ do
+                route $ setExtension "html"
+                compile $ pageCompiler
+                        >>> applyTemplateCompiler "templates/default.html"
+                        >>> relativizeUrlsCompiler
+
+        -- How-to page (w/ TOC)
+        match "howto.md" $ do
+                route $ setExtension "html"
+                compile $ pageCompilerWith defaultHakyllParserState withToc
+                        >>> applyTemplateCompiler "templates/default.html"
+                        >>> relativizeUrlsCompiler
+
+        -- Results page
+        match "results.md" $ do
         compile $ pageCompiler
             >>> applyTemplateCompiler "templates/results.html"
             >>> relativizeUrlsCompiler
